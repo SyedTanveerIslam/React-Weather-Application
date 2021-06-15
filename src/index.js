2d16fe5b24695be6194d016838802871663b8c46
Syed Tanveer Islam <rifattanveer2400@gmail.com>
2021-06-15 23:18:56 +0600

    Weather App first attempt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+/*const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position=>console.log(position),
+        err=>console.log(err)
+    );
+    return (
+        <div>Hi there!</div>
+    );
+};*/
+
+class App extends React.Component {
+    /*constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+    }*/
+
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderContent()
+    {
+        //Conditional Rendering
+        if (this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error:{this.state.errorMessage}</div>
+        }
+        return <Spinner message="Please accept Location Request"/>
+    }
+    //React says we have to define render!!!
+    render() {
+        return(
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
